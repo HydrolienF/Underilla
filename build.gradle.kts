777b17fe349ce85b2cd859f03d918040c18cd235
HydrolienF <hydrolien.f@gmail.com>
2025-12-15 02:57:54 +0100

    Update 1.21.10 datapack version & add back the previous datapack version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,10 @@ plugins {
 }
 
 group = "fr.formiko.mc.underilla"
-version = "2.1.9"
+version = "2.1.10"
 description="Generate vanilla cave in custom world."
-val mainMinecraftVersion = "1.21.9"
-val supportedMinecraftVersions = "1.21.3 - 1.21.9"
+val mainMinecraftVersion = "1.21.10"
+val supportedMinecraftVersions = "1.21.3 - 1.21.10"
 val voidWorldGeneratorVersion = "1.3.2"
 val chunkyVersion = "1.4.28"
 
